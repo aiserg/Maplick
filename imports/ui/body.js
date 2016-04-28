ea3a678793c40aac4b1480676f29105c0a4fa8d2
sineveel <sineveel@gmail.com>
2016-04-28 17:15:40 +0300

    add google maps

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -4,3 +4,21 @@ import { ReactiveDict } from 'meteor/reactive-dict';
 
 import './download.js'
 import './body.jade';
+
+Template.body.onRendered(function bodyOnCreated() {
+    let myLatlng = new google.maps.LatLng(51.741713, 36.194022);
+    let myOptions = {
+        zoom: 8,
+        center: myLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    
+    let map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+
+
+    let marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title:"Hello World!"
+    });
+});
