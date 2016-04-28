29ecd83152d9a31795d632fa26fbbeba7d3bb1be
sineveel <sineveel@gmail.com>
2016-04-28 20:27:33 +0300

    make fake work

--- imports/ui/download.js
+++ imports/ui/download.js
@@ -9,7 +9,21 @@ Template.download.events({
         FS.Utility.eachFile(event, function(file) {
             Images.insert(file, function (err, fileObj) {
                 console.log("----------------------------------", fileObj.collectionName + "-" + fileObj._id + "-" + fileObj.original.name);
-                let newPoint = new google.maps.LatLng(51.741713, 36.194022);
+                let x = 0, y = 0;
+                switch(fileObj.original.name) {
+                    case "rome.jpg":
+                        [x, y] = [41.891305, 12.492655];
+                        break;
+                    case "pushkinskiy.jpeg":
+                        [x, y] = [51.737959, 36.192831];
+                        break;
+                    case "tower.jpg":
+                        [x, y] = [48.858370, 2.294857];
+                        break;
+                    default:
+                        [x, y] = [51.741713, 36.194022];
+                }
+                let newPoint = new google.maps.LatLng(x, y);
                 let marker = new google.maps.Marker({
                     position: newPoint,
                     map: GLOBAL_MAP,
