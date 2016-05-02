be1e146d6158437a304e4193d44445efbbf01c48
aiserg <rishenes@gmail.com>
2016-05-02 10:53:16 +0300

    F*ck all semicolons

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,21 +1,21 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
 
 import './download.js'
-import './body.jade';
+import './body.jade'
 
-export let GLOBAL_MAP = null;
+export let GLOBAL_MAP = null
 
 
 Template.body.onRendered(function bodyOnCreated() {
-    let myLatlng = new google.maps.LatLng(-33.932861, 151.189182);
+    let myLatlng = new google.maps.LatLng(-33.932861, 151.189182)
     let myOptions = {
         zoom: 15,
         center: myLatlng,
         mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    GLOBAL_MAP = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-});
+    }
+    GLOBAL_MAP = new google.maps.Map(document.getElementById("map_canvas"), myOptions)
+})
 
 Template.body.events({
 
--- imports/ui/download.js
+++ imports/ui/download.js
@@ -1,7 +1,7 @@
-import { Meteor } from 'meteor/meteor';
-import { Template } from 'meteor/templating';
-import { Images } from '../api/images.js';
-import './download.jade';
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
+import { Images } from '../api/images.js'
+import './download.jade'
 import { GLOBAL_MAP } from './body.js'
 
 Template.download.events({
@@ -12,31 +12,30 @@ Template.download.events({
             let imgcontainer = document.getElementsByClassName('img-container')
             mainform[0].style.top = '15%'
             foundedimg[0].style.height = '70%'
-
             Images.insert(file, function (err, fileObj) {
-                console.log("----------------------------------", fileObj.collectionName + "-" + fileObj._id + "-" + fileObj.original.name);
-                let x = 0, y = 0;
+                console.log("----------------------------------", fileObj.collectionName + "-" + fileObj._id + "-" + fileObj.original.name)
+                let x = 0, y = 0
                 switch(fileObj.original.name) {
                     case "rome.jpg":
-                        [x, y] = [41.891305, 12.492655];
-                        break;
+                        [x, y] = [41.891305, 12.492655]
+                        break
                     case "pushkinskiy.jpeg":
-                        [x, y] = [51.737959, 36.192831];
-                        break;
+                        [x, y] = [51.737959, 36.192831]
+                        break
                     case "tower.jpg":
-                        [x, y] = [48.858370, 2.294857];
-                        break;
+                        [x, y] = [48.858370, 2.294857]
+                        break
                     default:
-                        [x, y] = [51.741713, 36.194022];
+                        [x, y] = [51.741713, 36.194022]
                 }
-                let newPoint = new google.maps.LatLng(x, y);
+                let newPoint = new google.maps.LatLng(x, y)
                 let marker = new google.maps.Marker({
                     position: newPoint,
                     map: GLOBAL_MAP,
                     title:"Hello World!"
-                });
-                GLOBAL_MAP.setCenter(newPoint);
-            });
-        });
+                })
+                GLOBAL_MAP.setCenter(newPoint)
+            })
+        })
     }
-});
+})
